aee515adb0c000baf55b2aa42ed384864f1e8c14
Kyu Sung Sohn <rok.ksohn@gmail.com>
2023-05-18 15:45:30 +0900

    Fix: import dependency cycle error

--- components/index.js
+++
@@ -1,8 +0,0 @@
-/* eslint-disable import/extensions */
-export { default as Modal } from './Modal.js';
-// eslint-disable-next-line import/no-cycle
-export { default as List } from './List.js';
-export { default as Header } from './Header.js';
-export { default as Card } from './Card.js';
-export { default as Board } from './Board.js';
-export { default as AddListButton } from './AddListButton.js';
--- index.js
+++ index.js
@@ -2,4 +2,4 @@
 import App from './App.js';
 import render from './utils/render.js';
 
-render(new App().render(), document.getElementById('root'));
+render(App, document.getElementById('root'));
