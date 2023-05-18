57c8eb09328db386263188cd647c01673bc264af
Kyu Sung Sohn <rok.ksohn@gmail.com>
2023-05-18 15:48:58 +0900

    Fix: AddListButton component invoke location

--- components/Board.js
+++ components/Board.js
@@ -1,12 +1,14 @@
 /* eslint-disable import/extensions */
 import Component from '../core/Component.js';
-import { List } from './index.js';
+import List from './List.js';
+import AddListButton from './AddListButton.js';
 
 class Board extends Component {
   render() {
     return `
       <div class="board">
         ${new List(this.props).render()}
+        ${new AddListButton(this.props).render()}
       </div>
     `;
   }
